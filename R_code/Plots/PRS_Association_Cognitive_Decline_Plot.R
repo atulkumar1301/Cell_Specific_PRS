4dacbab1fc56f6dacd88d60e5208dd53cbadc891
ATUL KUMAR <atul.0298@gmail.com>
2024-11-18 16:57:05 +0100

    Update PRS_Association_Cognitive_Decline_Plot.R

--- R_code/Plots/PRS_Association_Cognitive_Decline_Plot.R
+++ R_code/Plots/PRS_Association_Cognitive_Decline_Plot.R
@@ -7,8 +7,7 @@ library(ggpubr)
 cbbPalette <- c("#999999", "#F0E442", "#E69F00", "#009E73", "#CC79A7", "#0072B2", "#D55E00", "#56B4E9", "#000000")
 cbbPalette_1 <- c("#E69F00", "#009E73", "#CC79A7", "#0072B2", "#D55E00", "#56B4E9", "#000000")
 
-#df <- fread ("/Volumes/ATUL_6TB/Work/Projects/Cell_Specific_PRSs/Full_Data/Manuscript/PRS_Plots/PRS_Plot_APOE_MMSE.txt")
-df <- fread ("PRS_Plot_APOE_MMSE.txt")
+df <- fread ("/Volumes/ATUL_6TB/Work/Projects/Cell_Specific_PRSs/Full_Data/Manuscript/PRS_Plots/PRS_Plot_APOE_MMSE.txt")
 p1 <- ggplot (data = df, aes (x = Model, y = -log10(P), col = Cell_Type, label = Effect)) + geom_path() +
   geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
 p1 <- p1 + theme_bw() + scale_color_manual(values=cbbPalette)
@@ -31,8 +30,8 @@ p1 <- p1 +
         panel.background = element_blank()) + labs(title=expression("a) PRS Plot: Including APOE region variants"))
 
 p3 <- ggplot(df, aes(x = Model, y = Effect, col = Cell_Type)) +
-  geom_point(size = 4) +
-  geom_errorbar(aes(ymax = U95, ymin = L95, col = Cell_Type))
+  geom_point(position = position_dodge(0.7)) +
+  geom_errorbar(aes(ymax = U95, ymin = L95, col = Cell_Type), position = position_dodge(0.7), width = 0.2)
 p3 <- p3 + theme_bw() + scale_color_manual(values=cbbPalette_1)
 p3 <- p3 + scale_x_continuous (breaks=seq(1, 7, 1), labels=c("PRS 1", "PRS 2", "PRS 3", "PRS 4", "PRS 5", "PRS 6", "PRS 7"))
 p3 <- p3 + scale_y_continuous (breaks=seq(-0.8, 0.3, 0.05))
@@ -49,8 +48,7 @@ p3 <- p3 +
         panel.background = element_blank()) + labs(title=expression("c) Confidence Interval Plot: Including APOE region variants"))
 
 #### Non-APOE region
-#df_1 <- fread ("/Volumes/ATUL_6TB/Work/Projects/Cell_Specific_PRSs/Full_Data/Manuscript/PRS_Plots/PRS_Plot_Non_APOE_MMSE.txt")
-df_1 <- fread ("PRS_Plot_Non_APOE_MMSE.txt")
+df_1 <- fread ("/Volumes/ATUL_6TB/Work/Projects/Cell_Specific_PRSs/Full_Data/Manuscript/PRS_Plots/PRS_Plot_Non_APOE_MMSE.txt")
 p2 <- ggplot (data = df_1, aes (x = Model, y = -log10(P), col = Cell_Type, label = Effect)) + geom_path() +
   geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
 p2 <- p2 + theme_bw() + scale_color_manual(values=cbbPalette)
@@ -73,8 +71,8 @@ p2 <- p2 +
         panel.background = element_blank()) + labs(title=expression("b) PRS Plot: Excluding APOE region variants"))
 
 p4 <- ggplot(df_1, aes(x = Model, y = Effect, col = Cell_Type)) +
-  geom_point(size = 4) +
-  geom_errorbar(aes(ymax = U95, ymin = L95, col = Cell_Type))
+  geom_point(position = position_dodge(0.7)) +
+  geom_errorbar(aes(ymax = U95, ymin = L95, col = Cell_Type), position = position_dodge(0.7), width = 0.2)
 p4 <- p4 + theme_bw() + scale_color_manual(values=cbbPalette_1)
 p4 <- p4 + scale_x_continuous (breaks=seq(1, 7, 1), labels=c("PRS 1", "PRS 2", "PRS 3", "PRS 4", "PRS 5", "PRS 6", "PRS 7"))
 p4 <- p4 + scale_y_continuous (breaks=seq(-0.66, 0.08, 0.05))
