59b564c673e10776c21e1f72056183620aa621e4
ATUL KUMAR <atul.0298@gmail.com>
2024-11-25 15:26:03 +0100

    Update and rename PRS_CSF_pTau217_Assocoation_Plot_Old.R to PRS_CSF_pTau217_Assocoation_Plot.R

--- R_code/Plots/PRS_CSF_pTau217_Assocoation_Plot.R
+++ R_code/Plots/PRS_CSF_pTau217_Assocoation_Plot.R
@@ -1,18 +1,22 @@
+#! /Library/Frameworks/R.framework/Versions/4.2/Resources/bin/Rscript
 library(data.table)
-library(ggplot2)
+library (ggplot2)
+library(tidytext)
+library(tidyverse)
 library(ggrepel)
 library(ggpubr)
 
-#### APOE region
 cbbPalette <- c("#999999", "#F0E442", "#E69F00", "#009E73", "#CC79A7", "#0072B2", "#D55E00", "#56B4E9", "#000000")
-cbbPalette_1 <- c("#E69F00", "#009E73", "#CC79A7", "#0072B2", "#D55E00", "#56B4E9", "#000000")
+cbbPalette1 <- c("#E69F00", "#009E73", "#CC79A7", "#0072B2", "#D55E00", "#56B4E9", "#000000")
 
-df <- fread ("/Volumes/ATUL_6TB/Work/Projects/Cell_Specific_PRSs/Full_Data/Manuscript/PRS_Plots/PRS_Plot_APOE_MMSE.txt")
+#### APOE region
+df <- fread ("/Volumes/ATUL_6TB/Work/Projects/Cell_Specific_PRSs/Full_Data/Manuscript/PRS_Plots/PRS_Plot_APOE_CSF_pTau217.txt")
+
 p1 <- ggplot (data = df, aes (x = Model, y = -log10(P), col = Cell_Type, label = Effect)) + geom_path() +
   geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
 p1 <- p1 + theme_bw() + scale_color_manual(values=cbbPalette)
 p1 <- p1 + scale_x_continuous (breaks=seq(1, 7, 1), labels=c("PRS 1", "PRS 2", "PRS 3", "PRS 4", "PRS 5", "PRS 6", "PRS 7"))
-p1 <- p1 + scale_y_continuous (breaks=seq(0, 10, 1))
+p1 <- p1 + scale_y_continuous (breaks=seq(0, 25, 2))
 p1 <- p1 + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="#F0E442")) +
   geom_hline (aes (yintercept=-log10(1.0E-03), linetype = "Bonferroni p-value 0.05", col="#999999")) +
   scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
@@ -29,13 +33,20 @@ p1 <- p1 +
         legend.text = element_text(family = "serif", size=12),
         panel.background = element_blank()) + labs(title=expression("a) PRS Plot: Including APOE region variants"))
 
-p3 <- ggplot(df, aes(x = Model, y = Effect, col = Cell_Type)) +
-  geom_point(position = position_dodge(0.7)) +
-  geom_errorbar(aes(ymax = U95, ymin = L95, col = Cell_Type), position = position_dodge(0.7), width = 0.2)
-p3 <- p3 + theme_bw() + scale_color_manual(values=cbbPalette_1)
+df <- df %>% 
+  mutate(category = reorder_within(Cell_Type, AIC, within = Model))
+
+p3 <- ggplot(data=df, aes(x=Model, y=AIC, fill = Cell_Type, group = category)) +
+  geom_bar(stat="identity", position = position_dodge(0.9), width = 0.5) 
+
+p3 <- p3 + theme_bw() + scale_fill_manual(values=cbbPalette1)
+
+p3 <- p3 + coord_cartesian(ylim=c(3435,3535)) + scale_y_continuous(breaks = seq(3435,3535, by=10))
+
 p3 <- p3 + scale_x_continuous (breaks=seq(1, 7, 1), labels=c("PRS 1", "PRS 2", "PRS 3", "PRS 4", "PRS 5", "PRS 6", "PRS 7"))
-p3 <- p3 + scale_y_continuous (breaks=seq(-0.8, 0.3, 0.05))
-p3 <- p3 + xlab ("PRS Models") + labs (color = "Cell_Type") + ylab (expression (beta))
+
+p3 <- p3 + xlab ("PRS Models") + labs (color = "Cell_Type") + ylab (expression ("AIC"))
+
 p3 <- p3 +
   theme(legend.position="right",
         plot.title = element_text(family = "serif", size=14, face = "bold", hjust = 0.5),
@@ -45,15 +56,16 @@ p3 <- p3 +
         axis.text.y = element_text(family = "serif", size=12),
         legend.title = element_text(family = "serif", size=12),
         legend.text = element_text(family = "serif", size=12),
-        panel.background = element_blank()) + labs(title=expression("c) Confidence Interval Plot: Including APOE region variants"))
+        panel.background = element_blank()) + labs(title=expression("c) Bar Plot (AIC): Including APOE region variants"))
 
 #### Non-APOE region
-df_1 <- fread ("/Volumes/ATUL_6TB/Work/Projects/Cell_Specific_PRSs/Full_Data/Manuscript/PRS_Plots/PRS_Plot_Non_APOE_MMSE.txt")
+df_1 <- fread ("/Volumes/ATUL_6TB/Work/Projects/Cell_Specific_PRSs/Full_Data/Manuscript/PRS_Plots/PRS_Plot_Non_APOE_CSF_pTau217.txt")
+
 p2 <- ggplot (data = df_1, aes (x = Model, y = -log10(P), col = Cell_Type, label = Effect)) + geom_path() +
   geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
 p2 <- p2 + theme_bw() + scale_color_manual(values=cbbPalette)
 p2 <- p2 + scale_x_continuous (breaks=seq(1, 7, 1), labels=c("PRS 1", "PRS 2", "PRS 3", "PRS 4", "PRS 5", "PRS 6", "PRS 7"))
-p2 <- p2 + scale_y_continuous (breaks=seq(0, 7, 1))
+p2 <- p2 + scale_y_continuous (breaks=seq(0, 20, 2))
 p2 <- p2 + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="#F0E442")) +
   geom_hline (aes (yintercept=-log10(1.0E-03), linetype = "Bonferroni p-value 0.05", col="#999999")) +
   scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
@@ -69,14 +81,22 @@ p2 <- p2 +
         legend.title = element_text(family = "serif", size=12),
         legend.text = element_text(family = "serif", size=12),
         panel.background = element_blank()) + labs(title=expression("b) PRS Plot: Excluding APOE region variants"))
+p2
 
-p4 <- ggplot(df_1, aes(x = Model, y = Effect, col = Cell_Type)) +
-  geom_point(position = position_dodge(0.7)) +
-  geom_errorbar(aes(ymax = U95, ymin = L95, col = Cell_Type), position = position_dodge(0.7), width = 0.2)
-p4 <- p4 + theme_bw() + scale_color_manual(values=cbbPalette_1)
+df_1 <- df_1 %>% 
+  mutate(category = reorder_within(Cell_Type, AIC, within = Model))
+
+p4 <- ggplot(data=df_1, aes(x=Model, y=AIC, fill = Cell_Type, group = category)) +
+  geom_bar(stat="identity", position = position_dodge(0.9), width = 0.5) 
+
+p4 <- p4 + theme_bw() + scale_fill_manual(values=cbbPalette1)
+
+p4 <- p4 + coord_cartesian(ylim=c(3265,3335)) + scale_y_continuous(breaks = seq(3265,3335, by=5))
+
 p4 <- p4 + scale_x_continuous (breaks=seq(1, 7, 1), labels=c("PRS 1", "PRS 2", "PRS 3", "PRS 4", "PRS 5", "PRS 6", "PRS 7"))
-p4 <- p4 + scale_y_continuous (breaks=seq(-0.66, 0.08, 0.05))
-p4 <- p4 + xlab ("PRS Models") + labs (color = "Cell_Type") + ylab (expression (beta))
+
+p4 <- p4 + xlab ("PRS Models") + labs (color = "Cell_Type") + ylab (expression ("AIC"))
+
 p4 <- p4 +
   theme(legend.position="right",
         plot.title = element_text(family = "serif", size=14, face = "bold", hjust = 0.5),
@@ -86,9 +106,8 @@ p4 <- p4 +
         axis.text.y = element_text(family = "serif", size=12),
         legend.title = element_text(family = "serif", size=12),
         legend.text = element_text(family = "serif", size=12),
-        panel.background = element_blank()) + labs(title=expression("d) Confidence Interval Plot: Excluding APOE region variants"))
+        panel.background = element_blank()) + labs(title=expression("d) Bar Plot (AIC): Excluding APOE region variants"))
 
 plot <- ggarrange (p1, p2, p3, p4, ncol = 2, nrow = 2, common.legend = TRUE, legend = "bottom")
 
-
-annotate_figure(plot, top = text_grob("Cell-Specific PRS association with Cognitive Decline:MMSE-Baseline: BioFINDER-1", color = "black", face = "bold", size = 16, family = "serif"))
+annotate_figure(plot, top = text_grob("Cell-Specific PRS association with CSF pTau217: BioFINDER-1", color = "black", face = "bold", size = 16, family = "serif"))
